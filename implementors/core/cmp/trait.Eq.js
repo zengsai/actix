d4473c5f92355be319d5d644ff545dc6604dc5f9
Travis CI User <travis@example.org>
2018-05-17 17:11:13 +0000

    Documentation upload

---
+++ implementors/core/cmp/trait.Eq.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["actix"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"actix/struct.SpawnHandle.html\" title=\"struct actix::SpawnHandle\">SpawnHandle</a>",synthetic:false,types:["actix::actor::SpawnHandle"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"actix/actors/struct.Resolve.html\" title=\"struct actix::actors::Resolve\">Resolve</a>",synthetic:false,types:["actix::actors::resolver::Resolve"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"actix/actors/struct.Connect.html\" title=\"struct actix::actors::Connect\">Connect</a>",synthetic:false,types:["actix::actors::resolver::Connect"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"actix/actors/struct.ConnectAddr.html\" title=\"struct actix::actors::ConnectAddr\">ConnectAddr</a>",synthetic:false,types:["actix::actors::resolver::ConnectAddr"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/failure/trait.Fail.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["actix"] = [{text:"impl Fail for <a class=\"enum\" href=\"actix/enum.MailboxError.html\" title=\"enum actix::MailboxError\">MailboxError</a>",synthetic:false,types:["actix::address::MailboxError"]},{text:"impl Fail for <a class=\"enum\" href=\"actix/actors/enum.ConnectorError.html\" title=\"enum actix::actors::ConnectorError\">ConnectorError</a>",synthetic:false,types:["actix::actors::resolver::ConnectorError"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
